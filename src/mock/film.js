85d7cfc52fafee491871dafe07878bfa27ebf34b
Aleksandr Leontev <cruiser76_git@mail.ru>
2020-08-18 10:42:53 +0600

    Добавляет моки и заполняет компоненты на основе моков

---
+++ src/mock/film.js
@@ -0,0 +1,45 @@
+import {getRandomInt} from './../utils.js';
+
+const getRandomTitle = () => {
+  const titles = [`Jojo Rabbit`, `Jumanji: The Next Level`, `Avengers: Infinity War`, `Top Gun`, `Birdman or (The Unexpected Virtue of Ignorance)`, `Becky`, `Blitz`, `Harry Potter and the Half-Blood Prince`];
+  return titles[getRandomInt(0, titles.length - 1)];
+};
+
+const getRandomImg = () => {
+  const images = [`made-for-each-other.png`, `popeye-meets-sinbad.png`, `sagebrush-trail.jpg`, `santa-claus-conquers-the-martians.jpg`, `the-dance-of-life.jpg`, `the-great-flamarion.jpg`, `the-man-with-the-golden-arm.jpg`];
+  return images[getRandomInt(0, images.length - 1)];
+};
+
+const lorem = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus`;
+
+const getRandomPhrase = () => {
+  let description = ``;
+  for (let i = 1; i <= getRandomInt(1, 5); i++) {
+    description += ` ${lorem.split(`.`)[getRandomInt(0, lorem.split(`.`).length - 1)]}.`;
+  }
+  return description.trim();
+};
+
+const getComments = () => {
+  return {
+    author: `John Doe`,
+    date: new Date(),
+    message: getRandomPhrase()
+  };
+};
+
+const getFilm = () => {
+  const comments = Array(getRandomInt(0, 5)).fill().map(() => getComments());
+
+  return {
+    title: getRandomTitle(),
+    poster: `./images/posters/${getRandomImg()}`,
+    description: getRandomPhrase(),
+    comments,
+    isWatched: Boolean(getRandomInt(0, 1)),
+    isFavorite: Boolean(getRandomInt(0, 1)),
+    isInWatchlist: Boolean(getRandomInt(0, 1)),
+  };
+};
+
+export {getFilm};
--- src/view/site-menu.js
+++ src/view/site-menu.js
@@ -1,11 +1,20 @@
-const createMenuTemplate = () => {
+const createFiterItem = (filter, count) => {
+  const filterTitle = `${filter[0].toUpperCase()}${filter.slice(1)}${filter === `all` ? ` movies` : ``}`;
+  const countTemplate = filter === `all` ? `` : ` <span class="main-navigation__item-count">${count}</span>`;
+  const activeClass = filter === `all` ? `main-navigation__item--active` : ``;
+
+  return `<a href="#${filter}" class="main-navigation__item ${activeClass}">${filterTitle}${countTemplate}</a>`;
+};
+
+const createMenuTemplate = (filters = {}) => {
+  const filtersList = filters.map(({filter, count}) => {
+    return createFiterItem(filter, count);
+  }).join(``);
+
   return (
     `<nav class="main-navigation">
       <div class="main-navigation__items">
-        <a href="#all" class="main-navigation__item main-navigation__item--active">All movies</a>
-        <a href="#watchlist" class="main-navigation__item">Watchlist <span class="main-navigation__item-count">13</span></a>
-        <a href="#history" class="main-navigation__item">History <span class="main-navigation__item-count">4</span></a>
-        <a href="#favorites" class="main-navigation__item">Favorites <span class="main-navigation__item-count">8</span></a>
+        ${filtersList}
       </div>
       <a href="#stats" class="main-navigation__additional">Stats</a>
     </nav>`
