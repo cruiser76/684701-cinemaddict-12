897b5866c8e298386edc67fa1adc891e5818b833
Aleksandr Leontev <cruiser76_git@mail.ru>
2020-08-19 17:41:24 +0600

    Добавлены методы добавления дом элементов, создан класс для кнопки

--- src/main.js
+++ src/main.js
@@ -3,10 +3,11 @@ import {createMenuTemplate} from './view/site-menu.js';
 import {createSortTemplate} from './view/sort.js';
 import {createFilmsWrapper} from './view/films-wrapper.js';
 import {createFilmCard} from './view/card.js';
-import {createShowMoreTemplate} from './view/show-more.js';
+import ShowMore from './view/show-more.js';
 import {createStatisticsTemplate} from './view/statistics.js';
 import {getFilm} from './mock/film.js';
 import {getFilters} from './mock/filters.js';
+import {renderTemplate, RenderPosition, render} from './utils.js';
 
 const FILMS_COUNT = 22;
 const FILMS_COUNT_IN_ROW = 5;
@@ -14,17 +15,13 @@ const FILMS_COUNT_IN_ROW = 5;
 const films = Array(FILMS_COUNT).fill().map(() => getFilm());
 const filters = getFilters(films);
 
-const render = (container, template, place) => {
-  container.insertAdjacentHTML(place, template);
-};
-
 const header = document.querySelector(`.header`);
-render(header, createUserInfoTemplate(), `beforeEnd`);
+renderTemplate(header, createUserInfoTemplate(), `beforeEnd`);
 
 const main = document.querySelector(`.main`);
-render(main, createMenuTemplate(filters), `beforeEnd`);
-render(main, createSortTemplate(), `beforeEnd`);
-render(main, createFilmsWrapper(), `beforeEnd`);
+renderTemplate(main, createMenuTemplate(filters), `beforeEnd`);
+renderTemplate(main, createSortTemplate(), `beforeEnd`);
+renderTemplate(main, createFilmsWrapper(), `beforeEnd`);
 
 
 const filmsList = main.querySelector(`.films-list`);
@@ -32,12 +29,12 @@ const filmsList = main.querySelector(`.films-list`);
 
 const filmContainer = filmsList.querySelector(`.films-list__container`);
 for (let i = 0; i < Math.min(films.length, FILMS_COUNT_IN_ROW); i++) {
-  render(filmContainer, createFilmCard(films[i]), `beforeEnd`);
+  renderTemplate(filmContainer, createFilmCard(films[i]), `beforeEnd`);
 }
 
 if (films.length > FILMS_COUNT_IN_ROW) {
   let currentFilmsInRow = FILMS_COUNT_IN_ROW;
-  render(filmsList, createShowMoreTemplate(), `beforeEnd`);
+  render(filmsList, new ShowMore().getElement(), RenderPosition.BEFOREEND);
 
   const loadMoreButton = filmsList.querySelector(`.films-list__show-more`);
 
@@ -45,7 +42,7 @@ if (films.length > FILMS_COUNT_IN_ROW) {
     evt.preventDefault();
     films
       .slice(currentFilmsInRow, currentFilmsInRow + FILMS_COUNT_IN_ROW)
-      .forEach((film) => render(filmContainer, createFilmCard(film), `beforeEnd`));
+      .forEach((film) => renderTemplate(filmContainer, createFilmCard(film), `beforeEnd`));
 
     currentFilmsInRow += FILMS_COUNT_IN_ROW;
 
@@ -58,12 +55,12 @@ if (films.length > FILMS_COUNT_IN_ROW) {
 
 const extraFilms = main.querySelectorAll(`.films-list--extra`);
 const mostCommentedContainer = extraFilms[1].querySelector(`.films-list__container`);
-render(mostCommentedContainer, createFilmCard(films[0]), `beforeEnd`);
-render(mostCommentedContainer, createFilmCard(films[0]), `beforeEnd`);
+renderTemplate(mostCommentedContainer, createFilmCard(films[0]), `beforeEnd`);
+renderTemplate(mostCommentedContainer, createFilmCard(films[0]), `beforeEnd`);
 
 const topRatingContainer = extraFilms[0].querySelector(`.films-list__container`);
-render(topRatingContainer, createFilmCard(films[0]), `beforeEnd`);
-render(topRatingContainer, createFilmCard(films[0]), `beforeEnd`);
+renderTemplate(topRatingContainer, createFilmCard(films[0]), `beforeEnd`);
+renderTemplate(topRatingContainer, createFilmCard(films[0]), `beforeEnd`);
 
 const footer = document.querySelector(`.footer`);
-render(footer, createStatisticsTemplate(), `beforeEnd`);
+renderTemplate(footer, createStatisticsTemplate(), `beforeEnd`);
--- src/utils.js
+++ src/utils.js
@@ -3,3 +3,40 @@ export const getRandomInt = (a = 1, b = 0) => {
   const upper = Math.floor(Math.max(a, b));
   return Math.floor(lower + Math.random() * (upper - lower + 1));
 };
+
+export const createElement = (template) => {
+  const elementWrapper = document.createElement(`div`);
+  elementWrapper.innerHTML = template;
+
+  return elementWrapper.firstChild;
+};
+
+export const renderTemplate = (container, template, place) => {
+  container.insertAdjacentHTML(place, template);
+};
+
+export const RenderPosition = {
+  AFTERBEGIN: `afterbegin`,
+  BEFOREEND: `beforeend`,
+  AFTER: `after`,
+  BEFORE: `before`
+};
+
+
+export const render = (container, component, position) => {
+  switch (position) {
+    case RenderPosition.AFTERBEGIN:
+      container.prepend(component);
+      break;
+    case RenderPosition.BEFOREEND:
+      container.append(component);
+      break;
+    case RenderPosition.AFTER:
+      container.after(component);
+      break;
+
+    case RenderPosition.BEFORE:
+      container.before(component);
+      break;
+  }
+};
