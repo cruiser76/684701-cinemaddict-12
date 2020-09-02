43b8abdff2c849b0c0fcd53529f7498f298b1841
Aleksandr Leontev <cruiser76_git@mail.ru>
2020-09-02 21:40:21 +0600

    Добавляет момент js

--- webpack.config.js
+++ webpack.config.js
@@ -7,9 +7,12 @@ module.exports = {
     filename: 'bundle.js',
     path: path.join(__dirname, 'public'),
   },
-devtool: 'source-map',
-devServer: {
-  contentBase: path.join(__dirname, 'public'),
-  watchContentBase: true,
-  }
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    watchContentBase: true,
+  },
+  plugins: [
+    new MomentLocalesPlugin()
+  ]
 };
